c1fceac44d16d29ce97b08d4c6dbd9eaf6c88e76
Gniewogniew <jakubbajer83@gmail.com>
2017-07-02 14:54:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,11 @@ var main = function() {
 };
 
 $(document).ready(main);
+
+function roll()
+{
+  var roll1,roll2
+  roll1=parseInt(Math.random()*6+1)
+roll2=parseInt(Math.random()*6+1)
+document.getElementById("dice").innerHTML=roll1+" , "+roll2
+}
