cb565f49468f7a30e43864e1a9de04b05054a954
Gniewogniew <jakubbajer83@gmail.com>
2017-07-05 20:38:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var isMenuOpen = false;
 function main() {
   $(".icon-menu").click(function(){ 
     if(isMenuOpen){
-      $('.menu').animate({
+      $(".menu").animate({
       left: "-185px"
     }, 500);
     }else{
-        $('.menu').animate({
+        $(".menu").animate({
       left: "0px"
     }, 500);
         }
