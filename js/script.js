d4c6a61337314be6445e3b1e20aff01e21ce0522
Gniewogniew <jakubbajer83@gmail.com>
2017-07-06 18:53:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
-var isMenuOpen = false;
-
-function main() {
-  $(".icon-menu").click(function(){ 
-    if(isMenuOpen){
-      $('.menu').animate({
-      left: "-185px"
-    }, 500);
-    }else{
-        $('.menu').animate({
-      left: "0px"
-    }, 500);
-        }
-        isMenuOpen =!isMenuOpen;}
-    )}
-$(document).ready(main)
+$(document).ready(function(){
+    $(".icon-menu").click(function(){
+        $(".menu").toggle();
+    });
+});
