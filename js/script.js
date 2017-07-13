1777e935de20d1396ce32262d80623b09fddbf5a
Gniewogniew <jakubbajer83@gmail.com>
2017-07-13 16:05:22 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $(".icon-menu").click(function(){
-        $(".menu").slideToggle();
-    });
-});
+var isMenuOpen = false;
+
+function main() {
+  $(".icon-menu").click(function() {
+    if (isMenuOpen) {
+      $('body').animate({
+        left: "0px"
+        }, 500);
+      $(".menu").slideToggle();
+    }else{
+      $('body').animate({
+        left: "185px"
+        }, 500);
+      $(".menu").slideToggle();
+        }
+     isMenuOpen = !isMenuOpen;
+    })
+}
+$(document).ready(main)
