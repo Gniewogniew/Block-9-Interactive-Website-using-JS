5b64ca3309528993e3a1a14fb59e2afe13508a1f
Gniewogniew <jakubbajer83@gmail.com>
2017-07-13 17:07:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var isMenuOpen = false;
 function main() {
   $(".icon-menu").click(function() {
     if (isMenuOpen) {
-       $("body").toggleClass("move",false)
+       $("body").toggleClass("move",false);
+       $("body").toggleClass("moveback",true);
        $(".menu").slideToggle();
     }else{
        $("body").toggleClass("move",true);
+       $("body").toggleClass("moveback",false);
        $(".menu").slideToggle();
         }
    isMenuOpen = !isMenuOpen;
